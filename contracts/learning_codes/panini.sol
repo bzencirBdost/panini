ada669a2bbf09ab01a67ff029279617a749acbbb
bzencirBdost <baris.zencir@bdost.com>
2018-03-31 19:24:05 +0300

    Changed code to use ERC721
    + added Zeppelin library.
    + Changed code to use erc721 token standart.

--- contracts/learning_codes/panini.sol
+++ contracts/learning_codes/panini.sol
@@ -8,9 +8,9 @@ pragma solidity ^0.4.20;
 //###               CARDS               ###
 //#########################################
 
-library AnimalCard {
+library AnimalCardBase {
     
-    enum Geography {
+    enum Region {
         ASIA,
         AFRICA,
         NOURTH_AMERICA,
@@ -20,92 +20,141 @@ library AnimalCard {
         AUSTRALIA,
         OCEAN
     }
-    
-    //maybe?
-    enum Element {
-        EARTH, // land creatures
-        WATER, // water creatures
-        AIR, // birds
-        FIRE // insect
+   
+    //data
+    struct Data {
+        
+        //kullanilmayacak, bunun yerine indexler id olacak.
+        uint256 id; // arr index: 0: empty initialized and never used.
+        string name;
+        uint256 health; //life expectancy
+        uint256 weigth; //str
+        uint256 speed; //dex
+        Region region;
+        uint256 rarity; // 0-100 arasinda bir sayi. 
     }
+    
+    //returns clone of base card.
+    function clone(AnimalCardBase.Data self) returns (AnimalCardBase.Data) internal view {
+        return AnimalCardBase.Data(self.id, self.name, self.health, self.weigth, self.speed, self.region, self.rarity);
+    }
+
+
+}
+
+
+//kartlara ozel bazi farkliliklar katilabilir? 
+//Mesela ayni tip kart'in ozellikleri random degisebilir %5 gibi?
+//price burada tutulabilir bu durumda vs.
+library AnimalCard {
     
     //data
     struct Data {
-        uint id; //not: id = arr index + 1 , ipfs icin gerekli.
-        string name;
-        uint health; //life expectancy
-        uint weigth; //str
-        uint speed; //dex
-        Geography geography;
-        Element element; // family
-        uint rarity; // 0-100 arasinda bir sayi. 
+        
+        uint256 baseId; // arr index: 0: empty initialized and never used.
+        uint256 tokenId; 
+        address ownerId; //playerId
     }
-    
 }
 
 contract UsingCard {
+
+    using AnimalCardBase for AnimalCardBase.Data;
     using AnimalCard for AnimalCard.Data;
-    AnimalCard.Data[] animalCards;
-    mapping(uint => uint) numberOfAnimalCardCreated;
-    uint random;
+
+    //TO DO: NFT nft; //eklenecek. bunun metodlari iceride kullanilacak.
+
+    // metedata
+    AnimalCardBase.Data[] animalCardBase; 
+
+    // created cards: metedata ile token'i bagliyor.
+    //Normal token listesinden farki.
+    //
+    //tokenid(index = tokenId) -> baseid, + tokenid (app'da ise yarayabilir.)
+    AnimalCard.Data[] animalCards; 
+    //farkli kart tipleri icin uretilen tokenlar
+    // baseId -> tokenId list -> index of animalCards 
+    mapping(uint256 => uint256[]) tokensOfEachCards;
+
+    //for test
+    uint256 random;
     
     function UsingCard() public {
         random = 1;
-        initAnimalCards();
-    }
-    
-    function initAnimalCards() internal {
-        addAnimalCard('fil', 1000, 3000, 60, AnimalCard.Geography.AFRICA, AnimalCard.Element.EARTH, 14 );        
-        addAnimalCard('at', 400, 500, 90, AnimalCard.Geography.ASIA, AnimalCard.Element.EARTH, 22 );        
-        addAnimalCard('tavsan', 20, 2, 80, AnimalCard.Geography.EUROPE, AnimalCard.Element.EARTH, 55 );        
-        addAnimalCard('aslan', 200, 200, 80, AnimalCard.Geography.AFRICA, AnimalCard.Element.EARTH, 21 );        
-
-        addAnimalCard('balina', 10000, 30000, 40, AnimalCard.Geography.OCEAN, AnimalCard.Element.WATER, 14 );        
-        addAnimalCard('yunus', 1000, 300, 80, AnimalCard.Geography.OCEAN, AnimalCard.Element.WATER, 25 );        
-        addAnimalCard('kilic baligi', 100, 40, 140, AnimalCard.Geography.OCEAN, AnimalCard.Element.WATER, 5 );        
-
-        addAnimalCard('kartal', 100, 15, 100, AnimalCard.Geography.ASIA, AnimalCard.Element.AIR, 25 );        
-        addAnimalCard('guvercin', 10, 1, 30, AnimalCard.Geography.SOUTH_AMERICA, AnimalCard.Element.AIR, 24 );        
-
-        addAnimalCard('karinca', 1, 1, 1, AnimalCard.Geography.EUROPE, AnimalCard.Element.FIRE, 43 );        
+        initAnimalCardBase();
+        AnimalCard.Data memory emptyAnimalCard = AnimalCard.Data(0, 0, 0);        
+        animalCards.push(emptyAnimalCard);
 
     }
     
-    function isExistAnimalCard(uint index ) public view returns(bool) {
-        if(animalCards.length <= index) {
-            return false;
+    function initAnimalCardBase() internal {
+        //id'lerin indexler ile ayni olmasi icin eklendi. Kullanilmayacak. bunun id'si 0.
+        createAnimalCardBase('empty', 0, 0, 0, AnimalCard.Region.AFRICA, 0 );        
+        createAnimalCardBase('fil', 1000, 3000, 60, AnimalCard.Region.AFRICA, 14 );        
+        createAnimalCardBase('at', 400, 500, 90, AnimalCard.Region.ASIA, 22 );        
+        createAnimalCardBase('tavsan', 20, 2, 80, AnimalCard.Region.EUROPE, 55 );        
+        createAnimalCardBase('aslan', 200, 200, 80, AnimalCard.Region.AFRICA, 21 );        
+
+        createAnimalCardBase('balina', 10000, 30000, 40, AnimalCard.Region.OCEAN, 14 );        
+        createAnimalCardBase('yunus', 1000, 300, 80, AnimalCard.Region.OCEAN, 25 );        
+        createAnimalCardBase('kilic baligi', 100, 40, 140, AnimalCard.Region.OCEAN, 5 );        
+
+        createAnimalCardBase('kartal', 100, 15, 100, AnimalCard.Region.ASIA, 25 );        
+        createAnimalCardBase('guvercin', 10, 1, 30, AnimalCard.Region.SOUTH_AMERICA, 24 );        
+
+        createAnimalCardBase('karinca', 1, 1, 1, AnimalCard.Region.EUROPE, 43 );        
+
+    }
+    
+    // usuable id between 1 to (n-1)
+    function existAnimalCardBase(uint256 id ) public view returns(bool) {
+        if( id > 0 && id <= animalCardBase.length) {
+            return true;
         }
-        return true;                
+        return false;                
     }
 
-    function addAnimalCard(string _name, uint _health, uint _weigth, uint _speed, AnimalCard.Geography _geography, AnimalCard.Element _element, uint _rarity ) internal{
-        uint id = animalCards.length + 1;
-        animalCards.push(AnimalCard.Data(id, _name, _health, _weigth, _speed, _geography, _element, _rarity));
+    //usuable card id starts with 1.
+    function createAnimalCardBase(string _name, uint256 _health, uint256 _weigth, uint256 _speed, AnimalCard.Region _region, uint256 _rarity ) internal{
+        uint256 id = animalCardBase.length;
+        animalCardBase.push(AnimalCardBase.Data(id, _name, _health, _weigth, _speed, _region, _rarity));
     }
     
-    function generateRandomCardId(uint _number_of_stars) internal returns (uint) {
+    //returns index of animalCardBase
+    function generateRandomCardId(uint256 _number_of_stars) internal returns (uint256) {
         //private test for random
-        uint id = random;
-        uint extra = _number_of_stars + 2;
-        uint delta = animalCards.length;
+        uint256 id = random;
+        uint256 extra = _number_of_stars + 2;
+        uint256 delta = animalCardBase.length -1;
         require(delta > 0);
 
-        id = (id + 2) * (3*id + 2) + extra;
-        while( id >= delta) { // id min 0 olmali
-            id = id - delta;
-        }
-        numberOfAnimalCardCreated[id] = numberOfAnimalCardCreated[id] + 1;
-        random = id;
+        id =( ( ( id + 2 ) * ( 3 * id + 2 ) + extra ) % delta ) + 1;
+
         return id;
 
         // random'u nasil yapacagiz buna karar verilecek.
         //kartlar eklenirken bir islem yapilacak. random card genereate ederken secim buna gore. bir array'den index cikartilacak.
     }
     
-    function getCardFromId(uint id) internal view returns (AnimalCard.Data) {
-        AnimalCard.Data memory card = animalCards[id];
-        return AnimalCard.Data(card.id, card.name, card.health, card.weigth, card.speed, card.geography, card.element, card.rarity);
+    //cart üretilmesi için.
+    //token kullanacak.
+    //to: token icin. 
+    //baseId: random generated value of basecard index. 
+    function mintCardWithId(address _to, uint256 baseId) internal returns(uint256){
+
+        uint256 memory tokenId = animalCards.length;
+        //to do: add erc721 mint. (_to, id);
+        // set baseId of metedata
+        AnimalCard.Data memory animalCard = AnimalCard.Data(baseId, tokenId);        
+        animalCards.push(animalCard); 
+        tokensOfEachCards[baseId].push(tokenId);
+        return tokenId;
     }
+    
+    function getAnimalCardBase(uint256 id) internal view returns(AnimalCardBase) {
+        return animalCardBase[id].clone();
+    }
+            
 }
 
 
@@ -113,15 +162,17 @@ contract UsingCard {
 //###            PLAYERS                ###
 //#########################################
 library Player {
+
     struct Data {
-        uint id;
+        uint256 id; // adress olabilir, yada id kalsın. bakarız.
         string name;
-        uint number_of_stars;
-        mapping(uint => uint) animalCards;
+        uint256 number_of_stars;
+        //baseId -> cardIndex
+        mapping(uint256 => uint256[]) animalCards;    
     }
     
-    function hasCard(Data storage self, uint index ) public view returns(bool) {
-        if(self.animalCards[index] == 0) {
+    function hasCard(Data storage self, uint256 id ) public view returns(bool) {
+        if(self.animalCards[id].length == 0) {
             return false;
         }
         return true;                
@@ -132,7 +183,9 @@ library Player {
 contract UsingPlayer is UsingCard{
     using Player for Player.Data;
     mapping (address => Player.Data) players;
-    uint number_of_player;
+    mapping (uint256 => address) playerIdToAddress;
+    
+    uint256 number_of_player;
     
     function UsingPlayer() public {
         number_of_player = 0;
@@ -149,15 +202,18 @@ contract UsingPlayer is UsingCard{
     
     function addRandomCardToPlayer(address _address) internal {
         isPlayer(_address);
-        uint card_index = generateRandomCardId(players[_address].number_of_stars);
-        players[_address].animalCards[card_index] = players[_address].animalCards[card_index] + 1;
+        uint256 baseId = generateRandomCardId(players[_address].number_of_stars);
+        //kart'in üretilmesi.
+        uint256 tokenId = mintCardWithId(_address, baseId);
+        players[_address].animalCards[baseId] = tokenId;
+        animalCards[tokenId].ownerId = players[_address].id;        
         players[_address].number_of_stars = players[_address].number_of_stars + 1; // simdilik kart sayisi olsun. 
     }
     
     function register(string _name) public {
         require(players[msg.sender].id == 0);
-        number_of_player = number_of_player +1;
-        players[msg.sender].id = number_of_player;
+        number_of_player = number_of_player + 1;
+        players[msg.sender].id = number_of_player; 
         players[msg.sender].name = _name;
         //5x card 
         addRandomCardToPlayer(msg.sender);
@@ -172,32 +228,36 @@ contract UsingPlayer is UsingCard{
     }
   
     // name and sayisi
-    function getMyCard(uint index) __isPlayer public view returns(string, uint) {
-        uint count = 0;
-        string memory name = "Err";
-        
-        bool cont = isExistAnimalCard(index);
-        if (cont) {
-            name = animalCards[index].name;                    
-            cont = cont && players[msg.sender].hasCard(index);
+    function getMyCard(uint256 tokenId) __isPlayer public view returns(string, uint256, string) {
+        uint256 count = 0;
+        string memory name = "";        
+        string memory err = "";        
+        //kart var mı yani token var mi? : token yok ise baseId = 0 gelecektir.
+        uint256 baseId = animalCards[tokenId].baseId;
+        if (existAnimalCardBase(baseId)) {
+            name = animalCardBase[baseId].name;                    
+            //cont = players[msg.sender].hasCard(baseId);
+            //kartin sahibi mi?
+            if( players[msg.sender].id == animalCards[tokenId].id) {
+                count = players[msg.sender].animalCards[baseId];           
+
+            } else {
+                //TO DO: get message from struct of messages.
+                err = "You are not owner of this card."; 
+            }
         } else {
-            //throw err
+            //TO DO: get message from struct of messages.
+                err = "There is not card with this id."; 
         }
 
-        if(cont ) {
-            count = players[msg.sender].animalCards[index];           
-        }
-        
-        return (name, count);        
+        return (name, count, err);        
     }
 
-    function getMyAllCardIds() __isPlayer public view returns(uint[]) {
+    function getMyAllCardIds() __isPlayer public view returns(uint256[]) {
         
-        uint[] memory cards = new uint[](animalCards.length);
-        for(uint i = 0; i < animalCards.length; i++) {
-            if(players[msg.sender].hasCard(i)) {
-                cards[i] = players[msg.sender].animalCards[i];
-            }
+        uint256[] memory cards = new uint256[](animalCardBase.length);
+        for(uint256 i = 1; i < animalCardBase.length; i++) {
+            cards[i] = players[msg.sender].animalCards[i].length;
         }
         return cards;
     }
@@ -209,11 +269,11 @@ contract UsingPlayer is UsingCard{
 contract Panini is UsingPlayer{
 
     function Panini() public {
-    
         
     }
     
     function addPackageToPlayerTest(address _address) public payable {
+
         addRandomCardToPlayer(_address);
         addRandomCardToPlayer(_address);
         addRandomCardToPlayer(_address);
---
+++ contracts/panini/panini_erc721_token.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.4.20;
+
+//TO DO:
+// extend contact from zeppelin.erc721Token:
+// add path 
